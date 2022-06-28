50726ee296bafa60ffccbc0d95a532f9406de5e4
Ann Duong <thuyanannduong@gmail.com>
2022-06-27 23:34:30 -0400

    refactor to use hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import Navbar from './Navbar'
 import Player from './Player'
 import Game from './game/Game'
@@ -8,23 +8,19 @@ import InstructionsModal from './InstructionsModal'
 import EndGameModal from './EndGameModal'
 import {updateBoard} from './logic/game-logic'
 
-class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      board: this.resetBoard(),
-      currentPlayer: 1,
-      gameOver: false,
-      winner: null,
-      player1Score: 0,
-      player2Score: 0,
-      hoverColumn: null,
-      showInstructions: false,
-      showEndGame: false,
-    }
-  }
+function App() {
 
-  resetBoard(){
+  let [board, setBoard] = useState(resetBoard())
+  let [currentPlayer, setCurrentPlayer] = useState(1)
+  let [gameOver, setGameOver] = useState(false)
+  let [winner, setWinner] = useState(null)
+  let [player1Score, setPlayer1Score] = useState(0)
+  let [player2Score, setPlayer2Score] = useState(0)
+  let [hoverColumn, setHoverColumn] = useState(null)
+  let [showInstructions, setShowInstructions] = useState(false)
+  let [showEndGame, setShowEndGame] = useState(false)
+
+  function resetBoard(){
     return [
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
@@ -35,102 +31,90 @@ class App extends React.Component {
     ]
   }
 
-  handleHover = (hoverColumn) => {
-    if(!this.state.gameOver){
-      this.setState({hoverColumn})
+  function handleHover (hoverColumn) {
+    if(!gameOver){
+      setHoverColumn(hoverColumn)
     }
   }
 
-  handleTurn = () => {
-    if(!this.state.gameOver){
-      let [newBoard, valid, winner, draw] = updateBoard(this.state.board, this.state.hoverColumn, this.state.currentPlayer)
+  function handleTurn () {
+    if(!gameOver){
+      let [newBoard, valid, winner, draw] = updateBoard(board, hoverColumn, currentPlayer)
       if(winner){
         if(winner === 1){
-          this.setState(prevState => ({player1Score: prevState.player1Score + 1}))
+          setPlayer1Score(player1Score + 1)
         }else if (winner === 2) {
-          this.setState(prevState => ({player2Score: prevState.player2Score + 1}))
+          setPlayer2Score(player2Score + 1)
         }
-        this.setState({
-          board: newBoard,
-          currentPlayer: null,
-          gameOver: true,
-          hoverColumn: null,
-          winner: winner,
-          showEndGame: true,
-        })
+        setCurrentPlayer(null)
+        setGameOver(true)
+        setHoverColumn(null)
+        setWinner(winner)
+        setShowEndGame(true)
       }else if (draw) {
-        this.setState({
-          gameOver: true,
-          showEndGame: true,
-          hoverColumn: null,
-          board: newBoard
-        })
+        setGameOver(true)
+        setShowEndGame(true)
+        setHoverColumn(null)
       }else if(valid){
-        this.setState(prevState => ({
-          currentPlayer: prevState.currentPlayer === 1 ? 2 : 1,
-          board: newBoard
-        }))
+        setCurrentPlayer(currentPlayer === 1 ? 2 : 1)
       }
+      setBoard(newBoard)
     }
   }
 
-  toggleInstructions = () => {
-    this.setState(prevState => ({showInstructions: !prevState.showInstructions}))
+  function toggleInstructions() {
+    setShowInstructions(!showInstructions)
   }
 
-  closeModal = () => {this.setState({showEndGame: false})}
-
-  resetGame = () => {
-    this.setState({
-      board: this.resetBoard(),
-      currentPlayer: 1,
-      gameOver: false,
-      winner: null,
-      showEndGame: false,
-      showInstructions: false
-    })
+  function closeModal() {
+    setShowEndGame(false)
   }
 
-  render(){
-    let {board, gameOver, currentPlayer, player1Score, player2Score,
-      hoverColumn, showInstructions, showEndGame, winner} = this.state
-    return (
-      <div className="App">
-        <Navbar toggleInstructions={this.toggleInstructions}/>
-        <div className="ui stackable centered grid ">
-          <Player
-            currentPlayer={currentPlayer}
-            gameOver={gameOver}
-          />
-          <Game
-            board={board}
-            hoverColumn={hoverColumn}
-            handleHover={this.handleHover}
-            currentPlayer={currentPlayer}
-            handleTurn={this.handleTurn}
-          />
-          <Score
-            player1Score={player1Score}
-            player2Score={player2Score}
-          />
-          <Button
-            gameOver={gameOver}
-            resetGame={this.resetGame}
-          />
-          <InstructionsModal 
-            open={showInstructions}
-            onClose={this.toggleInstructions}
-          />
-          <EndGameModal
-            open={showEndGame}
-            onClose={this.closeModal}
-            winner={winner}
-            resetGame={this.resetGame}
-          />
-        </div>
+  function resetGame() {
+    setCurrentPlayer(1)
+    setGameOver(false)
+    setWinner(null)
+    setShowEndGame(false)
+    setShowInstructions(false)
+    setBoard(resetBoard())
+  }
+
+  return (
+    <div className="App">
+      <Navbar toggleInstructions={toggleInstructions}/>
+      <div className="ui stackable centered grid ">
+        <Player
+          currentPlayer={currentPlayer}
+          gameOver={gameOver}
+        />
+        <Game
+          board={board}
+          hoverColumn={hoverColumn}
+          handleHover={handleHover}
+          currentPlayer={currentPlayer}
+          handleTurn={handleTurn}
+        />
+        <Score
+          player1Score={player1Score}
+          player2Score={player2Score}
+        />
+        <Button
+          gameOver={gameOver}
+          resetGame={resetGame}
+        />
+        <InstructionsModal 
+          open={showInstructions}
+          onClose={toggleInstructions}
+        />
+        <EndGameModal
+          open={showEndGame}
+          onClose={closeModal}
+          winner={winner}
+          resetGame={resetGame}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
